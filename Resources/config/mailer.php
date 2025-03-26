7f969fae686679e4028b949c4ecea584d6bccb35
Kevin Bond <kevinbond@gmail.com>
2025-03-26 12:16:31 -0400

    Revert "[Mailer][TwigBridge] Add support for translatable subject"
    This reverts commit 97eb49be33232745a793084dcef8f0ef4674e8e6.

--- Resources/config/mailer.php
+++ Resources/config/mailer.php
@@ -18,11 +18,7 @@ use Symfony\Component\Mime\BodyRendererInterface;
 return static function (ContainerConfigurator $container) {
     $container->services()
         ->set('twig.mailer.message_listener', MessageListener::class)
-            ->args([
-                null,
-                service('twig.mime_body_renderer'),
-                '$translator' => service('translator')->ignoreOnInvalid(),
-            ])
+            ->args([null, service('twig.mime_body_renderer')])
             ->tag('kernel.event_subscriber')
 
         ->set('twig.mime_body_renderer', BodyRenderer::class)
