86ef8290d8c7b8dc8a2324e5a62d7fb96bda3135
Alexander M. Turek <me@derrabus.de>
2026-03-24 11:05:05 +0100

    [HttpKernel][FrameworkBundle] Deprecate Bundle::registerCommands()

--- TwigBundle.php
+++ TwigBundle.php
@@ -16,7 +16,6 @@ use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\ExtensionPass;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\RuntimeLoaderPass;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\TwigEnvironmentPass;
 use Symfony\Bundle\TwigBundle\DependencyInjection\Compiler\TwigLoaderPass;
-use Symfony\Component\Console\Application;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -38,10 +37,5 @@ class TwigBundle extends Bundle
         $container->addCompilerPass(new TwigEnvironmentPass());
         $container->addCompilerPass(new TwigLoaderPass());
         $container->addCompilerPass(new RuntimeLoaderPass(), PassConfig::TYPE_BEFORE_REMOVING);
-    }
-
-    public function registerCommands(Application $application): void
-    {
-        // noop
     }
 }
